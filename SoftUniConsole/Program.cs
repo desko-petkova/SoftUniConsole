38631ba45e557a9eeca8af7bf8f4722daa1364cf
PCK216 <PCK216@K216>
2026-04-15 08:44:17 +0300

    Add project files.

---
+++ SoftUniConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SoftUniConsole.Data.Models;
+
+namespace SoftUniConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new DbContextOptionsBuilder<SoftUniContext>()
+                .UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=SoftUni;TrustServerCertificate=False;Integrated Security=True;")
+                .Options;
+
+           using var db = new SoftUniContext(options);
+
+
+
+            var employees = db.Employees.Take(10).ToList();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} - {employee.Salary}");
+            }
+
+
+        }
+    }
+}
